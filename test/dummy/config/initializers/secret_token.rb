957e0f64795d1f8ab5969ca13ee79d374f516fc5
Lucas Renan <lucas@freelancersbrasil.com>
2011-03-27 14:06:01 -0300

    generated with enginex

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'a5b54d82472fa335e333e102cb4638a4c29a98cb88577b71bd72286ddf89f3c56cb28c9a728fdb8985f3de3c253c5f51f2d4164bd8bd1bf665560512f303dca1'
---
+++ test/publish_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PublishTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Publish
+  end
+end
