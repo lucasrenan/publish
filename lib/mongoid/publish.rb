d62e60ca12f5c36414f8b9bead433decdd92abc1
Lucas Renan <lucas@freelancersbrasil.com>
2011-04-01 15:30:05 -0300

    fixes the published? method; add publish! method

--- lib/mongoid/publish.rb
+++ lib/mongoid/publish.rb
@@ -1,28 +1,40 @@
 module Mongoid
   module Publish
     extend ActiveSupport::Concern
-    
+
     included do
       field :published_at, :type => Date
       field :published,    :type => Boolean, :default => false
-      
+
       before_save :set_published_at
     end
-    
+
     module InstanceMethods
+      def published?
+        if self.published && self.published_at && self.published_at <= Date.today
+          true
+        else
+          false
+        end
+      end
+
+      def publish!
+        self.published    = true
+        self.published_at = Date.today
+        self.save
+      end
+
       private
       def set_published_at
-        self.published_at = Time.now if self.published && self.published_at.nil?
+        self.published_at = Date.today if self.published && self.published_at.nil?
       end
     end
-    
+
     module ClassMethods
-      def published(user_signed_in = false)
-        result = desc(:published_at, :created_at)
-        result = result.where(:published => true, :published_at.lte => Time.now) unless user_signed_in
-        result
+      def published
+        where(:published => true, :published_at.lte => Date.today).desc(:published_at, :created_at)
       end
     end
-    
+
   end
 end
