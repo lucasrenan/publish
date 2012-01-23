0bc75fb0c010b0d83813a4b514de4796a3b1951e
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-01-23 14:47:59 -0200

    Removing InstanceMethods module inside of ActiveSupport::Concern because it is deprecated on ActiveSupport 3.2.0.

--- lib/mongoid/publish.rb
+++ lib/mongoid/publish.rb
@@ -12,29 +12,27 @@ module Mongoid
       before_save :set_published_at
     end
 
-    module InstanceMethods
-      def published?
-        if self.published && self.published_at && self.published_at <= Date.today
-          true
-        else
-          false
-        end
+    def published?
+      if self.published && self.published_at && self.published_at <= Date.today
+        true
+      else
+        false
       end
+    end
 
-      def publish!
-        self.published    = true
-        self.published_at = Date.today
-        self.save
-      end
-      
-      def publication_status
-        self.published? ? self.published_at : "draft"
-      end
+    def publish!
+      self.published    = true
+      self.published_at = Date.today
+      self.save
+    end
+    
+    def publication_status
+      self.published? ? self.published_at : "draft"
+    end
 
-      private
-      def set_published_at
-        self.published_at = Date.today if self.published && self.published_at.nil?
-      end
+    private
+    def set_published_at
+      self.published_at = Date.today if self.published && self.published_at.nil?
     end
 
     module ClassMethods
